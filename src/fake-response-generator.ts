67e19426ce5eaf35c27acd803e1306b01eca7ea3
Ryo Ota <nwtgck@gmail.com>
2018-08-29 21:00:51 +0900

    [refactor] Improve readability of `body` in `nginx()`

--- src/fake-response-generator.ts
+++ src/fake-response-generator.ts
@@ -3,7 +3,22 @@ import * as http from "http";
 export function nginx(res: http.ServerResponse): void {
   // TODO: Hard code nginx version
   // TODO: Hard code "<!-- a padding to disable MSIE and Chrome friendly error page -->"
-  const body = "<html>\r\n<head><title>500 Internal Server Error</title></head>\r\n<body bgcolor=\"white\">\r\n<center><h1>500 Internal Server Error</h1></center>\r\n<hr><center>nginx/1.15.2</center>\r\n</body>\r\n</html>\r\n<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n";
+  // (INFO: Ruby one-liner(localhost:8181 is an actual Nginx Server): puts `curl -i -H 'User-Agent: Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1)' localhost:8081`.split("\r\n").map{|e| (e+"\r\n").inspect}.join(" +\n"))
+  const body =
+    "<html>\r\n" +
+    "<head><title>500 Internal Server Error</title></head>\r\n" +
+    "<body bgcolor=\"white\">\r\n" +
+    "<center><h1>500 Internal Server Error</h1></center>\r\n" +
+    "<hr><center>nginx/1.15.2</center>\r\n" +
+    "</body>\r\n" +
+    "</html>\r\n" +
+    "<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n" +
+    "<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n" +
+    "<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n" +
+    "<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n" +
+    "<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n" +
+    "<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n";
+
   res.shouldKeepAlive = false;
   res.writeHead(500, "Internal Server Error", {
     // TODO: Hard code nginx version
