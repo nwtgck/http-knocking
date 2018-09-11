a9ed9c0fcc53d7ccf5d514aafbcb78386c8528ff
Ryo Ota <nwtgck@gmail.com>
2018-09-11 21:22:11 +0900

    [add] Create `PromiseHttpServer` class to test WebHook

--- test/test-util.ts
+++ test/test-util.ts
@@ -1,6 +1,7 @@
 import * as websocket from 'websocket';
 import * as WebSocket from 'ws';
 import * as net from 'net';
+import * as http from 'http';
 
 // Sleep
 // (from: https://qiita.com/yuba/items/2b17f9ac188e5138319c)
@@ -127,4 +128,94 @@ export function httpGet(host: string, port: number, getPath: string, headers: {[
       reject(err);
     });
   });
+}
+
+/**
+ * Unwrap union type of T and undefined
+ * @param value
+ */
+function unwrapUndefined<T>(value: T | undefined): T {
+  return value as T;
+}
+
+type ReqRes = {req: http.IncomingMessage, res: http.ServerResponse};
+type ReqResOrError =
+  {
+    kind: "REQ_RES",
+    req: http.IncomingMessage,
+    res: http.ServerResponse
+  } |
+  {
+    kind: "ERROR",
+    error: Error
+  };
+
+export class PromiseHttpServer {
+  private resolveAndRejectQueue: ({resolve: (reqRes: ReqRes) => void, reject: (err: Error) => void})[] = [];
+  private reqResOrErrorQueue: ReqResOrError[] = [];
+
+  readonly server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse)=>{
+    if(this.resolveAndRejectQueue.length === 0) {
+      // Push request and response
+      this.reqResOrErrorQueue.push({
+        kind: "REQ_RES",
+        req: req,
+        res: res
+      });
+    } else {
+      // Get the first resolve and reject functions
+      const resolveAndReject: {resolve: (reqRes: ReqRes) => void, reject: (err: Error) => void}
+        = unwrapUndefined(this.resolveAndRejectQueue.shift());
+      // Resolve
+      resolveAndReject.resolve({
+        req: req,
+        res: res
+      })
+    }
+  }).on('error', (err) => {
+    if(this.resolveAndRejectQueue.length == 0) {
+      // Push error
+      this.reqResOrErrorQueue.push({
+        kind: "ERROR",
+        error: err
+      });
+    } else {
+      // Get the first resolve and reject functions
+      const resolveAndReject: {resolve: (reqRes: ReqRes) => void, reject: (err: Error) => void}
+        = unwrapUndefined(this.resolveAndRejectQueue.shift());
+      // Resolve
+      resolveAndReject.reject(err);
+    }
+  });
+
+  /**
+   * Wait for request and response
+   */
+  reqRes(): Promise<ReqRes> {
+    return new Promise<ReqRes>(((resolve, reject) => {
+      if(this.reqResOrErrorQueue.length === 0) {
+        // Append resolver
+        this.resolveAndRejectQueue.push({
+          resolve: resolve,
+          reject: reject
+        });
+      } else {
+        // Get the first request and response
+        const reqResOrError: ReqResOrError = unwrapUndefined(this.reqResOrErrorQueue.shift());
+        switch (reqResOrError.kind) {
+          case "REQ_RES":
+            // Resolve by the request and response
+            resolve({
+              req: reqResOrError.req,
+              res: reqResOrError.res
+            });
+            break;
+          case "ERROR":
+            // Reject by error
+            reject(reqResOrError.error);
+            break;
+        }
+      }
+    }));
+  }
 }
